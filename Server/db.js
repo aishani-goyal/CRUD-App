8d8e368a7d8a2311b07168a7bc9d1527f41e728c
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-06-23 22:05:02 +0530

    Notes app

---
+++ Server/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb+srv://appuser:AppUser007@notes.qcjtsdo.mongodb.net/";
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB successfully');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
+
+module.exports = connectToMongo;
